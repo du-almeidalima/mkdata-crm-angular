a90b8bdf24e10cc498b919e652bbaa9c34b21e4b
du-almeidalima <dualmeidalima@gmail.com>
2020-05-22 10:17:18 -0300

    Modules Folders and Core Module

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,16 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+/**
+ * Esse module vai ter as principais funcionalidades do nosso App que são necessárias para ele funcionar
+ */
+@NgModule({
+  declarations: [],
+  exports: [
+
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoreModule {}
