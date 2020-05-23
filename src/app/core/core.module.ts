1f5a361f6feae610f9f26a84f3630cb7f79a0d54
Xenosgrilda <dualmeidalima@gmail.com>
2020-05-23 11:44:16 -0300

    Home Component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,18 +1,26 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {AuthModule} from "./auth/auth.module";
+import { HomeComponent } from './components/home/home.component';
+import {RouterModule, Routes} from "@angular/router";
+import {AuthGuard} from "./auth/auth.guard";
 
+const CORE_ROUTES: Routes = [
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: '', redirectTo: 'home', pathMatch: 'full'}
+]
 /**
  * Esse module vai ter as principais funcionalidades do nosso App que são necessárias para ele funcionar
  */
 @NgModule({
-  declarations: [],
+  declarations: [ HomeComponent ],
   exports: [
 
   ],
   imports: [
     CommonModule,
-    AuthModule
+    AuthModule,
+    RouterModule.forChild( CORE_ROUTES )
   ]
 })
 export class CoreModule {}
